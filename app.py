55a5e0a17ca983386e62bbd768aaa30f959eb623
dknowles <knowlesjarrett1@gmail.com>
2024-01-09 11:47:28 -0400

    setup pipFile
    Installed and setup pipFile

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+import os
 
+# Init App 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+
+@app.route('/', methods=['GET'])
+def get():
+    return jsonify({'msg': 'helloWorld'})
+
+# Run Server
+if __name__ == '__main__':
+    app.run(debug=True)
