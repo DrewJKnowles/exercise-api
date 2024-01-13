d4ac9cdc03f5065c0c13ed84539279114f66e59b
Drew Knowles <80469869+DrewJKnowles@users.noreply.github.com>
2024-01-13 08:46:19 -0400

    Update App.py and Created New Exercise Endpoint

--- app.py
+++ app.py
@@ -21,8 +21,9 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.register_blueprint(routes)
 
 db = SQLAlchemy(app)
+ma = Marshmallow(app)    
 
-ma = Marshmallow(app)
+
 
 class Excercise(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -43,27 +44,9 @@ class ExerciseSchema(Schema):
 exercise_schema = ExerciseSchema()    
 exercises_schema = ExerciseSchema(many=True)
 
-
-# class MuscleGroups(db.Model):
-#     muscle_group_id = db.Column(db.integer, primary_key=True)
-#     name = db.Column(db.String(100), unique=True)      
-    
-#     def __init__(self, name):
-#         self.name = name       
-         
-# class ExerciseGifs(db.Model):
-#     gif_id = db.Column(db.integer, primary_key=True)
-#     exercise_id = db.Column(db.integer)    
-#     description = db.Column(db.String(200))
-#     url = db.Column(db.String(200))  
-    
-#     def __init__(self, exercise_id, description, url):
-#         self.exercise_id = exercise_id  
-#         self.description = description
-#         self.url = url        
-      
-
-
+# Build DB Tables and Information 
+with app.app_context():
+    db.create_all()
 # Run Server
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ routes/exercises.py
@@ -0,0 +1,11 @@
+from flask import jsonify, request
+from . import routes
+from database
+
+@routes.route('/exercise', methods=['POST'])
+def add_exercise():
+    name = request.json['name']
+    description = request.json['description']
+    howToDescription = request.json['howToDescription']
+    
+    new-Exercise = Exercise()
